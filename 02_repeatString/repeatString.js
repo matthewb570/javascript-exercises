fdae7248ffd8fe20b37dba33dc50dd5816772176
Matthew Burgess <boog397@gmail.com>
2024-01-27 15:11:03 -0600

    Add solution for 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    let concatenatedString = "";
 
+    for (let i = 0; i < num; i++) {
+        concatenatedString += string;
+    }
+
+    return concatenatedString;
 };
 
 // Do not edit below this line
