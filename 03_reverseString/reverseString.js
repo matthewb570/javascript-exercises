b7df17dc0c4e3d925a1a9cc4d91e7a2ac35b73bc
Matthew Burgess <boog397@gmail.com>
2024-01-27 15:27:56 -0600

    Add solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reversedString = "";
+
+    let stringArray = string.split("");
+
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        reversedString += stringArray[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
