d6fc876ecf794bd9deb8ee42ab843c889ccdac8a
Matthew Burgess <boog397@gmail.com>
2024-01-27 15:55:27 -0600

    Add solution and additional test cases for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    for (let i = 0; i < args.length; i++) {
+        
+        let j = 0;
+        while (j < array.length) {
+            if (array[j] === args[i]) {
+                array.splice(j, 1);
+            } else {
+                j++;
+            }
+        }
+    }
 
+    return array;
+
+    // return array.filter(element => !args.includes(element));
 };
 
 // Do not edit below this line
