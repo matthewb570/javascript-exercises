3ba58aef1288c02c51be68346c1ba20a4f80c8f7
Matthew Burgess <boog397@gmail.com>
2024-02-12 21:21:51 -0600

    Add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    let phraseWithoutSpecialCharacters = phrase.replace(/[^a-zA-Z0-9]/g, "");
+    let phraseWithoutSpecialCharactersReversed = phraseWithoutSpecialCharacters.split("").reverse().join("");
 
+    return phraseWithoutSpecialCharacters.toLowerCase() === phraseWithoutSpecialCharactersReversed.toLowerCase();
 };
 
 // Do not edit below this line
