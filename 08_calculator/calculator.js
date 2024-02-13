0f594208895f1d7b92503dd75cf00c201bff4327
Matthew Burgess <boog397@gmail.com>
2024-02-12 20:43:14 -0600

    Add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((currentSum, number) => currentSum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((currentProduct, number) => currentProduct * number, 1);
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	let product = 1;
+  for (let i = 0; i < power; i++) {
+    product *= number;
+  }
+  return product;
 };
 
-const factorial = function() {
+const factorial = function(number) {
 	
+  let factorial = 1;
+  for (let i = number; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
