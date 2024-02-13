7e03fb29b12cf7cd29be6c826758b859494dc13c
Matthew Burgess <boog397@gmail.com>
2024-02-12 21:42:39 -0600

    Add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if (num <= 1) {
+        return +num;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
